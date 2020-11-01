f9283513a7fbea9a69d549e40600ff1237099bfd
Van <taikin.banno@gmail.com>
2020-11-01 15:54:56 +0900

    マイグレーション修正２

--- db/migrate/20201029130437_create_comments.rb
+++ db/migrate/20201029130437_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :text
+      t.text :text, null: false
       t.references :user, :prototype, foreign_key:true
+      t.references :prototype, foreign_key: true
       t.timestamps 
 
     end
